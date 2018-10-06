5b1ed62faafaa2e9a9045f715c644028f8726e5e
nick <nikki2lol@mail.ru>
2018-10-06 14:04:59 +0300

    Поправил названия переменных

--- js/filters.js
+++ js/filters.js
@@ -5,8 +5,8 @@ var PRICE_MAX = 1500;
 var SLIDER_WIDTH = 10; // или лучше PIN_WIDTH?
 var catalogRangeWrapperElement = document.querySelector('.catalog__filter.range');
 var rangeElement = catalogRangeWrapperElement.querySelector('.range__filter');
-var rangeLeftElement = rangeElement.querySelector('.range__btn--left');
-var rangeRightElement = rangeElement.querySelector('.range__btn--right');
+var rangeLeftPinElement = rangeElement.querySelector('.range__btn--left');
+var rangeRightPinElement = rangeElement.querySelector('.range__btn--right');
 var rangeLineElement = rangeElement.querySelector('.range__fill-line');
 var rangePriceMinElement = catalogRangeWrapperElement.querySelector('.range__price--min');
 var rangePriceMaxElement = catalogRangeWrapperElement.querySelector('.range__price--max');
@@ -22,21 +22,21 @@ var updatePrice = function (coordX) {
 // evt на разных уровнях я бы тоже переименовал как mouseDownEvt и mouseMoveEvt
 var onSliderMouseDown = function (evt) {
   evt.preventDefault();
-  var currentSlider = evt.target;
-  var startCoordX = currentSlider.offsetLeft;
+  var currentPin = evt.target;
+  var startCoordX = currentPin.offsetLeft;
 
   // мы вставили обработчик событий в обработчик событий, чтобы пока пишешь обработчик событий, мог писать обработчик событий
   var onSliderMouseMove = function (event) {
     event.preventDefault();
     var currentSliderPosition = startCoordX - (evt.clientX - event.clientX);
-    if (currentSlider === rangeLeftElement && currentSliderPosition < rangeRightElement.offsetLeft && currentSliderPosition >= 0) {
-      currentSlider.style.left = currentSliderPosition + 'px';
+    if (currentPin === rangeLeftPinElement && currentSliderPosition < rangeRightPinElement.offsetLeft && currentSliderPosition >= 0) {
+      currentPin.style.left = currentSliderPosition + 'px';
       rangeLineElement.style.left = currentSliderPosition + SLIDER_WIDTH / 2 + 'px';
       rangePriceMinElement.textContent = updatePrice(currentSliderPosition);
     }
 
-    if (currentSlider === rangeRightElement && currentSliderPosition > rangeLeftElement.offsetLeft && currentSliderPosition <= rangeWidth) {
-      currentSlider.style.left = currentSliderPosition + 'px';
+    if (currentPin === rangeRightPinElement && currentSliderPosition > rangeLeftPinElement.offsetLeft && currentSliderPosition <= rangeWidth) {
+      currentPin.style.left = currentSliderPosition + 'px';
       rangeLineElement.style.right = rangeWidth - currentSliderPosition + 'px';
       rangePriceMaxElement.textContent = updatePrice(currentSliderPosition);
     }
@@ -51,5 +51,5 @@ var onSliderMouseDown = function (evt) {
   document.addEventListener('mouseup', onSliderMouseUp);
 };
 
-rangeLeftElement.addEventListener('mousedown', onSliderMouseDown);
-rangeRightElement.addEventListener('mousedown', onSliderMouseDown);
+rangeLeftPinElement.addEventListener('mousedown', onSliderMouseDown);
+rangeRightPinElement.addEventListener('mousedown', onSliderMouseDown);
