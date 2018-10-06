689c5a512abdf5fc71d6901d65484ab831041c9c
nick <nikki2lol@mail.ru>
2018-10-06 15:30:58 +0300

    5.2 разбил код на модули

--- js/filters.js
+++ js/filters.js
@@ -1,55 +1,58 @@
 'use strict';
-// врееменно добавлю две константы для показа функицонала фильтра
-var PRICE_MIN = 100;
-var PRICE_MAX = 1500;
-var SLIDER_WIDTH = 10; // или лучше PIN_WIDTH?
-var catalogRangeWrapperElement = document.querySelector('.catalog__filter.range');
-var rangeElement = catalogRangeWrapperElement.querySelector('.range__filter');
-var rangeLeftPinElement = rangeElement.querySelector('.range__btn--left');
-var rangeRightPinElement = rangeElement.querySelector('.range__btn--right');
-var rangeLineElement = rangeElement.querySelector('.range__fill-line');
-var rangePriceMinElement = catalogRangeWrapperElement.querySelector('.range__price--min');
-var rangePriceMaxElement = catalogRangeWrapperElement.querySelector('.range__price--max');
-var rangeWidth = rangeElement.clientWidth;
+// модуль для работы с фильтром
+(function () {
+  var PRICE_MIN = 100;
+  var PRICE_MAX = 1500;
+  var SLIDER_WIDTH = 10; // или лучше PIN_WIDTH?
+  var catalogRangeWrapperElement = document.querySelector('.catalog__filter.range');
+  var rangeElement = catalogRangeWrapperElement.querySelector('.range__filter');
+  var rangeLeftPinElement = rangeElement.querySelector('.range__btn--left');
+  var rangeRightPinElement = rangeElement.querySelector('.range__btn--right');
+  var rangeLineElement = rangeElement.querySelector('.range__fill-line');
+  var rangePriceMinElement = catalogRangeWrapperElement.querySelector('.range__price--min');
+  var rangePriceMaxElement = catalogRangeWrapperElement.querySelector('.range__price--max');
+  var rangeWidth = rangeElement.clientWidth;
 
-// может не coordX а просто X, по идее понятно все равно будет, что это координата
-var updatePrice = function (coordX) {
-  var currentPosition = coordX * 100 / (rangeWidth - SLIDER_WIDTH);
-  return ((PRICE_MAX - PRICE_MIN) * currentPosition / 100 + PRICE_MIN).toFixed(0);
-};
-
-// как вариант назвать onSliderMouseDown/onPinMouseDown = function () {....}
-// evt на разных уровнях я бы тоже переименовал как mouseDownEvt и mouseMoveEvt
-var onSliderMouseDown = function (evt) {
-  evt.preventDefault();
-  var currentPin = evt.target;
-  var startCoordX = currentPin.offsetLeft;
-
-  // мы вставили обработчик событий в обработчик событий, чтобы пока пишешь обработчик событий, мог писать обработчик событий
-  var onSliderMouseMove = function (event) {
-    event.preventDefault();
-    var currentPinPosition = startCoordX - (evt.clientX - event.clientX);
-    if (currentPin === rangeLeftPinElement && currentPinPosition < rangeRightPinElement.offsetLeft && currentPinPosition >= 0) {
-      currentPin.style.left = currentPinPosition + 'px';
-      rangeLineElement.style.left = currentPinPosition + SLIDER_WIDTH / 2 + 'px';
-      rangePriceMinElement.textContent = updatePrice(currentPinPosition);
-    }
-
-    if (currentPin === rangeRightPinElement && currentPinPosition > rangeLeftPinElement.offsetLeft && currentPinPosition <= rangeWidth) {
-      currentPin.style.left = currentPinPosition + 'px';
-      rangeLineElement.style.right = rangeWidth - currentPinPosition + 'px';
-      rangePriceMaxElement.textContent = updatePrice(currentPinPosition);
-    }
+  // может не coordX а просто X, по идее понятно все равно будет, что это координата
+  var updatePrice = function (coordX) {
+    var currentPosition = coordX * 100 / (rangeWidth - SLIDER_WIDTH);
+    return ((PRICE_MAX - PRICE_MIN) * currentPosition / 100 + PRICE_MIN).toFixed(0);
   };
 
-  var onSliderMouseUp = function () {
-    document.removeEventListener('mousemove', onSliderMouseMove);
-    document.removeEventListener('mouseup', onSliderMouseUp);
+  // как вариант назвать onSliderMouseDown/onPinMouseDown = function () {....}
+  // evt на разных уровнях я бы тоже переименовал как mouseDownEvt и mouseMoveEvt
+  var onSliderMouseDown = function (evt) {
+    evt.preventDefault();
+    var currentPin = evt.target;
+    var startCoordX = currentPin.offsetLeft;
+
+    // мы вставили обработчик событий в обработчик событий, чтобы пока пишешь обработчик событий, мог писать обработчик событий
+    var onSliderMouseMove = function (event) {
+      event.preventDefault();
+      var currentPinPosition = startCoordX - (evt.clientX - event.clientX);
+      if (currentPin === rangeLeftPinElement && currentPinPosition < rangeRightPinElement.offsetLeft && currentPinPosition >= 0) {
+        currentPin.style.left = currentPinPosition + 'px';
+        rangeLineElement.style.left = currentPinPosition + SLIDER_WIDTH / 2 + 'px';
+        rangePriceMinElement.textContent = updatePrice(currentPinPosition);
+      }
+
+      if (currentPin === rangeRightPinElement && currentPinPosition > rangeLeftPinElement.offsetLeft && currentPinPosition <= rangeWidth) {
+        currentPin.style.left = currentPinPosition + 'px';
+        rangeLineElement.style.right = rangeWidth - currentPinPosition + 'px';
+        rangePriceMaxElement.textContent = updatePrice(currentPinPosition);
+      }
+    };
+
+    var onSliderMouseUp = function () {
+      document.removeEventListener('mousemove', onSliderMouseMove);
+      document.removeEventListener('mouseup', onSliderMouseUp);
+    };
+
+    document.addEventListener('mousemove', onSliderMouseMove);
+    document.addEventListener('mouseup', onSliderMouseUp);
   };
 
-  document.addEventListener('mousemove', onSliderMouseMove);
-  document.addEventListener('mouseup', onSliderMouseUp);
-};
+  rangeLeftPinElement.addEventListener('mousedown', onSliderMouseDown);
+  rangeRightPinElement.addEventListener('mousedown', onSliderMouseDown);
 
-rangeLeftPinElement.addEventListener('mousedown', onSliderMouseDown);
-rangeRightPinElement.addEventListener('mousedown', onSliderMouseDown);
+})();
---
+++ js/order.js
@@ -0,0 +1,170 @@
+'use strict';
+// модуль, содержащий логику формы оформления заказа
+(function () {
+  var ESC_KEYCODE = 27;
+  window.ESC_KEYCODE = ESC_KEYCODE;
+  var MIN_NUMBER_VALUE_KEYCODE = 48;
+  var MAX_NUMBER_VALUE_KEYCODE = 57;
+  var order = document.querySelector('.buy .order');
+
+  var paymentFormElement = order.querySelector('.payment');
+  var paymentMethodElement = paymentFormElement.querySelector('.payment__method');
+  var paymentCardWrapperElement = paymentFormElement.querySelector('.payment__card-wrap');
+  var paymentCardElement = paymentMethodElement.querySelector('#payment__card');
+  var paymentCashWrapperElement = paymentFormElement.querySelector('.payment__cash-wrap');
+  var paymentCashElement = paymentMethodElement.querySelector('#payment__cash');
+  var paymentInputElements = [].slice.call(paymentCardWrapperElement.querySelectorAll('input'));
+  var paymentRadioBtnElements = [].slice.call(paymentMethodElement.querySelectorAll('.toggle-btn__input'));
+  var paymentCardStatusElement = paymentFormElement.querySelector('.payment__card-status');
+  var cardInputElement = paymentFormElement.querySelector('#payment__card-number');
+  var cardDateElement = paymentFormElement.querySelector('#payment__card-date');
+  var cardCvcElement = paymentFormElement.querySelector('#payment__card-cvc');
+  var cardHolderElement = paymentFormElement.querySelector('#payment__cardholder');
+
+  var deliveryFormElement = order.querySelector('.deliver');
+  var deliveryMethodElement = deliveryFormElement.querySelector('.deliver__toggle');
+  var deliveryStoreElement = deliveryFormElement.querySelector('.deliver__store');
+  var deliveryStoreBtnElement = deliveryFormElement.querySelector('#deliver__store');
+  var deliveryCourierElement = deliveryFormElement.querySelector('.deliver__courier');
+  var deliveryCourierBtnElement = deliveryFormElement.querySelector('#deliver__courier');
+  var deliveryRadioBtnElements = [].slice.call(deliveryMethodElement.querySelectorAll('.toggle-btn__input'));
+  var deliveryStoreListElement = deliveryFormElement.querySelector('.deliver__store-list');
+  var deliveryStoreItemElements = [].slice.call(deliveryStoreListElement.querySelectorAll('.deliver__store-item'));
+  var deliveryStoreMapElement = deliveryFormElement.querySelector('.deliver__store-map-wrap');
+  var deliveryMapImageElement = deliveryStoreMapElement.querySelector('.deliver__store-map-img');
+
+  var successPopup = document.querySelector('#modal-success');
+
+  var onPopupEscPress = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      closeSuccessPopup();
+    }
+  };
+
+  var showSuccessPopup = function () {
+    successPopup.classList.remove('modal--hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+
+  var closeSuccessPopup = function () {
+    successPopup.classList.add('modal--hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+  };
+
+  // Блоки-переключатели
+  var togglePayment = function () {
+    paymentCardWrapperElement.classList.toggle('visually-hidden', paymentCashElement.checked);
+    paymentCashWrapperElement.classList.toggle('visually-hidden', paymentCardElement.checked);
+
+    paymentInputElements.forEach(function (elem) {
+      elem.disabled = paymentCashElement.checked;
+    });
+  };
+
+  var toggleDelivery = function () {
+    deliveryStoreElement.classList.toggle('visually-hidden', deliveryCourierBtnElement.checked);
+    deliveryCourierElement.classList.toggle('visually-hidden', deliveryStoreBtnElement.checked);
+
+    deliveryStoreElement.querySelector('.deliver__stores').disabled = deliveryCourierBtnElement.checked;
+    deliveryCourierElement.querySelector('.deliver__entry-fields-wrap').disabled = deliveryStoreBtnElement.checked;
+  };
+
+  // вводимое число - только цифра
+  var isNumber = function (event) {
+    if (event.keyCode < MIN_NUMBER_VALUE_KEYCODE || event.keyCode > MAX_NUMBER_VALUE_KEYCODE) {
+      event.returnValue = false;
+    }
+  };
+
+  var checkCardNumber = function (input) {
+    var array = input.value.split('').map(function (element, index) {
+      var numeral = parseInt(element, 10);
+      if (index === 0 || index % 2 === 0) {
+        var numeral2 = numeral * 2;
+        return numeral2 > 9 ? numeral2 - 9 : numeral2;
+      }
+      return numeral;
+    });
+
+    return array.reduce(function (x, y) {
+      return x + y;
+    }) % 10 === 0;
+  };
+
+  var validateCardData = function () {
+    paymentCardStatusElement.textContent = checkCardNumber(cardInputElement) ? 'Одобрен' : 'Не определён';
+    return checkCardNumber(cardInputElement);
+  };
+
+  var validate = function (evt) {
+    if (validateCardData()) {
+      evt.preventDefault();
+      showSuccessPopup();
+      successPopup.querySelector('.modal__close').addEventListener('click', closeSuccessPopup);
+    }
+  };
+
+  paymentRadioBtnElements.forEach(function (elem) {
+    elem.addEventListener('change', togglePayment);
+  });
+
+  deliveryRadioBtnElements.forEach(function (elem) {
+    elem.addEventListener('change', toggleDelivery);
+  });
+
+  deliveryStoreItemElements.forEach(function (elem) {
+    elem.addEventListener('click', function () {
+      deliveryMapImageElement.src = 'img/map/' + elem.querySelector('input').value + '.jpg';
+      deliveryMapImageElement.alt = elem.querySelector('label').textContent;
+    });
+  });
+
+  cardInputElement.addEventListener('keypress', isNumber);
+  cardCvcElement.addEventListener('keypress', isNumber);
+
+  cardInputElement.addEventListener('invalid', function () {
+    var errorText = '';
+    if (cardInputElement.validity.patternMismatch || cardInputElement.validity.tooShort || cardInputElement.validity.tooLong) {
+      errorText = 'Номер карты должен состоять только из 16 цифр';
+    } else if (cardInputElement.validity.valueMissing) {
+      errorText = 'Обязательное для заполнения поле';
+    }
+    cardInputElement.setCustomValidity(errorText);
+  });
+
+  cardDateElement.addEventListener('invalid', function () {
+    var errorText = '';
+    if (cardDateElement.validity.tooShort || cardDateElement.validity.tooLong || cardDateElement.validity.patternMismatch) {
+      errorText = 'Срок действия карты должен быть указан в формате ММ/ГГ';
+    } else if (cardDateElement.validity.valueMissing) {
+      errorText = 'Обязательное для заполнения поле';
+    }
+    cardDateElement.setCustomValidity(errorText);
+  });
+
+  cardCvcElement.addEventListener('invalid', function () {
+    var errorText = '';
+    if (cardCvcElement.validity.tooShort || cardCvcElement.validity.tooLong || cardCvcElement.validity.patternMismatch) {
+      errorText = 'Поле должно содержать 3 цифры';
+    } else if (cardCvcElement.validity.valueMissing) {
+      errorText = 'Обязательное для заполнения поле';
+    }
+    cardCvcElement.setCustomValidity(errorText);
+  });
+
+  cardHolderElement.addEventListener('invalid', function () {
+    var errorText = '';
+    if (cardHolderElement.validity.patternMismatch) {
+      errorText = 'Поле должно содержать только латинские буквы A-Z';
+    } else if (cardHolderElement.validity.valueMissing) {
+      errorText = 'Обязательное для заполнения поле';
+    }
+    cardHolderElement.setCustomValidity(errorText);
+  });
+
+  // Валидация форм
+  order.addEventListener('submit', function (evt) {
+    validate(evt);
+  });
+
+})();
